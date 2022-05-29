c486cc860d90407b309a9450e6834acb4947c102
WAHDINAAZIZAH <1902020005@mhs.univbinainsan.ac.id>
2022-05-29 14:25:54 +0700

    add register api

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -13,4 +13,34 @@ use function PHPUnit\Framework\isTrue;
 
 class AuthController extends Controller
 {
+    public $messageStatus = 200;
+
+    public function register(Request $request)
+    {
+
+        $validate = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json(['error' => $validate->errors()]);
+        }
+
+        $cekmail = User::where('email', $request->email)->first();
+
+        if ($cekmail) {
+            return response()->json(['error' => "Email Sudah digunakan"]);
+        } else {
+            $input = $request->all();
+            $input['password'] = bcrypt($input['password']);
+            $user = User::Create($input);
+            $success['token'] = $user->createToken('GlucosApp')->accessToken;
+            $success['name'] = $user->name;
+
+
+            return response()->json(['success' => $success], $this->messageStatus);
+        }
+    }
 }
